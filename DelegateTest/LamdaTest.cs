67cf73c6bcef34247647b5391e301350ec8864b5
Vijay Nadar <vijay.nadar@clariontechnologies.co.in>
2022-10-30 21:49:44 +0530

    Removed comments

--- DelegateTest/LamdaTest.cs
+++ DelegateTest/LamdaTest.cs
@@ -12,7 +12,7 @@ namespace DelegateTest
 
             for (int i = 0; i < 3; i++)
                 actions[i] = () => Console.Write(i);
-            foreach (Action a in actions) a(); // 333
+            foreach (Action a in actions) a(); 
         }
     }
 }
